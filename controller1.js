bb5e802f651567edcd2dc33f85f0fb2347367e79
preeti <preeti@Preeti.local>
2018-08-14 12:58:03 +0530

    temporary

--- controller1.js
+++ controller1.js
@@ -1,8 +1,8 @@
-angular.module("myapp").controller("personalInfo",[$scope,function($scope){
-     $scope.personalInfo={
+app.controller("personalInfo",function($scope){
+     varpersonalInformation={
          name        : Preeti,
          dateOfBirth : 01/11/1994,
          location    : Pune
      }
-}]);
-    console.log($scope.personalInfo);
+});
+    $scope.personalInformation=personalInformation;
